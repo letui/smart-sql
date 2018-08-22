6b89fb4ceaeccffb33b2ccac3f2bfdb8d58ae657
liyanbin <liyanbin@weishangtech.com>
2018-08-22 15:58:31 +0800

    完善函数对SQL拼接的支持

--- smart-sql/src/main/java/org/smartsql/ex/SQL.java
+++ smart-sql/src/main/java/org/smartsql/ex/SQL.java
@@ -32,56 +32,65 @@ public class SQL {
 	}
 	
 	public SQL where() {
-		this.buf.append("WHERE ");
+		this.buf.append("WHERE 1=1 ");
 		return this;
 	}
 	
 	public SQL leftJoin(String table) {
+		this.buf.append("LEFT JOIN "+table+" ");
 		return this;
 	}
 	public SQL rightJoin(String table) {
+		this.buf.append("RIGHT JOIN "+table+" ");
 		return this;
 	}
 	
 	public SQL innerJoin(String table) {
+		this.buf.append("INNER JOIN "+table+" ");
 		return this;
 	}
 	public SQL on(String columnSrc,String columnTarget) {
+		this.buf.append("ON "+columnSrc+" = "+columnTarget+" ");
 		return this;
 	}
 	
 	public SQL orEqual(String src,String target) {
+		this.buf.append("OR "+src+" = "+target+" ");
 		return this;
 	}
 	public SQL orLessThan(String src,String target) {
+		this.buf.append("OR "+src+" < "+target+" ");
 		return this;
 	}
 	public SQL orGreatThan(String src,String target) {
+		this.buf.append("OR "+src+" > "+target+" ");
 		return this;
 	}
 	
 	public SQL andEqual(String src,String target) {
-		if(!this.buf.toString().trim().endsWith("WHERE")) {
-			this.buf.append("AND ");
-		}
-		this.buf.append(src+" = "+target);
+		this.buf.append("AND "+src+" = "+target+" ");
 		return this;
 	}
 	public SQL andLessThan(String src,String target) {
+		this.buf.append("AND "+src+" < "+target+" ");
 		return this;
 	}
 	public SQL andGreatThan(String src,String target) {
+		this.buf.append("AND "+src+" > "+target+" ");
 		return this;
 	}
 	
 	public SQL groupBy(String strings) {
+		this.buf.append("GROUP BY "+strings+" ");
 		return this;
 	}
 	public SQL orderBy(String strings) {
+		this.buf.append("ORDER BY "+strings+" ");
 		return this;
 	}
 	
 	public SQL in(String strings) {
+		this.buf.append("in "+strings+" ");
 		return this;
 	}
 	@Override
